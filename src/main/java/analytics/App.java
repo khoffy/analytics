b20c8a5faabd744fdc64065837f26c50694bd6c7
khoffy <adolf_hermann86@yahoo.fr>
2023-07-24 04:57:34 -0500

    Project creation with 'mvn archetype:generate'

---
+++ src/main/java/analytics/App.java
@@ -0,0 +1,16 @@
+package analytics;
+
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.generated.StormTopology;
+import org.apache.storm.topology.TopologyBuilder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+
+    }
+}
